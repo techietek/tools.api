e02b9404a603fd149375b004e74a7ff93f05efa7
Techie <info@techietek.com>
2015-02-07 22:08:18 +0000

    removed production settings file and updated wsgi file

--- api/wsgi.py
+++ api/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+import sys
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(BASE_DIR)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'api.settings.production'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
